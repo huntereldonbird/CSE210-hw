1ab0a323cbfb84f30677a65418c551adbf52e31a
Eldon <Birdheldon@gmail.com>
2024-09-21 19:55:40 -0600

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,15 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    
+    static void Main(string[] args){
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your family name");
+        string familyName = Console.ReadLine();
+
+        Console.WriteLine("Your name is " + familyName + ", " + firstName + " " + familyName);
     }
+
+
 }
