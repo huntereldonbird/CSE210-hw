53852efd7319d75645dcabdaa38d4c377310fef4
Eldon <Birdheldon@gmail.com>
2024-09-27 21:59:00 -0600

    awdawdawd

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Diagnostics.Metrics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int Magic = new Random().Next(0, 11);
+        int guess = -1;
+        int Counter = 0;
+
+        do {
+
+            Counter++;
+            Console.Write("What is your guess :");
+            guess = int.Parse(Console.ReadLine());
+
+            if (Magic > guess){
+                Console.WriteLine("Higher");
+            }
+            else if (guess > Magic){
+                Console.WriteLine("Lower");
+            }
+            else{
+                Console.WriteLine($"You guessed right in {Counter} Moves");
+            }
+
+
+        }
+        while (guess != Magic);
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List<int> inputs = new List<int>();
+
+        int UserInput = -1;
+
+        do {
+
+            Console.Write("What number would you like to add?: ");
+            UserInput = int.Parse(Console.ReadLine());
+
+
+            if (UserInput == 0){
+
+                
+                int sum = 0;
+                foreach (int i in inputs){
+                    sum += i;
+                }
+                Console.WriteLine($"the sum is {sum}");
+
+                float avg = ((float)sum) / inputs.Count;
+
+                int largest = 0;
+                foreach (int x in inputs) {
+
+                    if (x > largest){
+                        largest = x;
+                    }
+                }
+
+                Console.WriteLine($"The Largest Number is: {largest}");
+
+            }
+            else{
+
+            }
+
+        }
+        while (UserInput != 0);
+
+
     }
+
 }
