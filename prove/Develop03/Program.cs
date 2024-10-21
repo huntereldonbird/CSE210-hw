f57ea3b4b5aa600aed985cb0a91e285186a44bac
Kyle Kerr <kkerr2550@gmail.com>
2024-10-21 14:43:47 -0600

    references to Referece renamed to Nicaea

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,7 +3,7 @@ using System;
 class Program {
     static void Main()
     {
-        Reference reference = new Reference();
-        reference.Main();
+        Nicaea Nicaea = new Nicaea();
+        Nicaea.Main();
     }
 }
