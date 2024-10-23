dd7c623f34c5913bc7df2ad0b8c9822ebcdb3b85
Kyle Kerr <kkerr2550@gmail.com>
2024-10-23 14:55:10 -0600

    beta test of the scripture dictionary

--- prove/Develop03/Verse.cs
+++ prove/Develop03/Verse.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Dynamic;
+using System.Collections.Generic;
 
 class Verse {
 
@@ -15,4 +16,11 @@ class Verse {
         _wholeVerse = set;
     }
 
+    // store a dictionary of Verses
+
+    private Dictionary<string, string> Scripture = new Dictionary<string, string>() {
+        {"D&C 10:5", "Pray always, that you may come off conqueror; yea, that you may conquer Satan, and that you may escape the hands of the servants of Satan that do uphold his work."},
+        {"D&C 1:37-38", "37 Search these commandments, for they are true and faithful, and the prophecies and promises which are in them shall all be fulfilled. \n38 What I the Lord have spoken, I have spoken, and I excuse not myself; and though the heavens and the earth pass away, my word shall not pass away, but shall all be fulfilled, whether by mine own voice or by the voice of my servants, it is the same."},
+        {"D&C 18:10-11", "10 Remember the worth of souls is great in the sight of God; \n11 For, behold, the Lord your Redeemer suffered death in the flesh; wherefore he suffered the pain of all men, that all men might repent and come unto him."}
+    };
 }
