f9a8a314acd92010128ef7ffcc77bf867e2b9579
EldonBird <Birdheldon@gmail.com>
2024-11-01 14:21:06 -0600

    yugyugyuuyg

--- prepare/Learning04/WritingAssignmnet.cs
+++ prepare/Learning04/WritingAssignmnet.cs
@@ -4,10 +4,19 @@ public class WritingAssignmnet: assignment
 {
     private String _title;
 
+    public String get_title(String title)
+    {
+        if (title != null)
+        {
+            _title = title;
+        }
+        
+        return _title;
+    }
+
 
     public String GetWritingInformation()
     {
-        
         return null;
     }
 }
