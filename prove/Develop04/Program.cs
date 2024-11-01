cf99d3d42e737ae34920b9c8cf5d9deb69a8bfab
EldonBird <Birdheldon@gmail.com>
2024-11-01 14:08:06 -0600

    kuguyuy

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,13 @@
 using System;
 
+using System.Media.SystemSounds;
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
     }
+    
+    SystemSounds.SoudPlayer
 }
