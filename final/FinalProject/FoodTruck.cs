8432eadc30782a8f852f117e7ed65b70e18fa2a7
EldonBird <Birdheldon@gmail.com>
2024-12-04 15:23:19 -0700

    addded some other stuff.

--- final/FinalProject/FoodTruck.cs
+++ final/FinalProject/FoodTruck.cs
@@ -1,8 +1,46 @@
 namespace FinalProject;
 
 public class FoodTruck {
+
 	
+	// does all of the logic when the food truck is started, hunter
+	public FoodTruck() {
+
+		Console.Clear();
+		Console.WriteLine("Welcome to the food truck");
+		
+		Console.WriteLine();
+		
+		Console.WriteLine("Which system do you want to use.");
+		Console.WriteLine("		1: Register");
+		Console.WriteLine("		2: Kitchen");
+		Console.WriteLine("		3: Expenditures");
+		
+		int userin = int.Parse(Console.ReadLine());
+
+		switch (userin) {
+			
+			case (1):
+
+				break;
+			
+			case (2):
+
+				break;
+			
+			case (3):
+
+				break;
+			
+		}
+		
+	}
+
 	
+	// starts up the expenditure class and adds all of the values, hunter
+	void load_Expenditure_System() {
+		
+	}
 	
 	
 }
--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,10 @@
 using System;
+using FinalProject;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        FoodTruck foodTruck = new FoodTruck();
     }
 }
