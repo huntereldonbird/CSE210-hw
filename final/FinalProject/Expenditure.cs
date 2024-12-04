c4d76b77ad01ec0abb8b4b8c28a49527451cbc29
Kyle Kerr <kkerr2550@gmail.com>
2024-12-04 14:50:01 -0700

    Initial Expenditure Dictionary

--- final/FinalProject/Expenditure.cs
+++ final/FinalProject/Expenditure.cs
@@ -1,5 +1,14 @@
 namespace FinalProject;
 
 public class Expenditure {
-	
+	public void DictionaryMenuItem = new Dictionary<MenuItem, int>() {
+        {"Oreos", 5},
+        {"Snickers", 5},
+        {"Butter", 5},
+        {"Ice Cream", 5},
+        {"Salad", 5},
+        {"Set of Chicken Tenders", 5},
+        {"Water", -1},
+        {"Baja Blast", 5}
+    };
 }
