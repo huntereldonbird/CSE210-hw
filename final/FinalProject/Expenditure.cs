78d6f1fd9cd617344d67901e436497b8d1f0394a
Kyle Kerr <kkerr2550@gmail.com>
2024-12-14 20:43:20 -0700

    aosngpauhpadfnhj

--- final/FinalProject/Expenditure.cs
+++ final/FinalProject/Expenditure.cs
@@ -2,7 +2,8 @@ namespace FinalProject;
 
 public class Expenditure {
 	
-	
+	FoodTruck _foodTruck;
+
 	public Dictionary<MenuItem, int> DictionaryMenuItem = new Dictionary<MenuItem, int>() {
 		
 		{new BajaBlast(), 5},
@@ -22,14 +23,9 @@ public class Expenditure {
 		return DictionaryMenuItem;
 		
 	}
-
+      
 	private FoodTruck _foodtruck;
 	
-    private void change_value(MenuItem menuItem, int value) {
-        
-	    DictionaryMenuItem[menuItem] = value;
-	    
-    }
 	public Expenditure(FoodTruck foodTruck) {
 
         _foodTruck = foodTruck;
@@ -45,22 +41,6 @@ public void BeginSession(int previousindex) {
         Console.Clear();
         
         Console.WriteLine("Expenditure System : ");
-
-        foreach (var ticket in _foodTruck.LoadTickets("./past/")) {
-            
-            Console.WriteLine("-----------------------");
-            
-            Console.Write(ticket.Get_orderid() + " : ");
-
-            foreach (var mi in ticket.Get_menu_items()) {
-                
-            }
-                        
-            Console.WriteLine();
-
-            
-            
-        }
         
         Console.WriteLine("-----------------------");
 
@@ -79,3 +59,49 @@ public void BeginSession(int previousindex) {
         }
     }   
 }
+
+public MenuItem ViewRecords(){
+			
+		string choice = "";
+			Console.WriteLine("What would you like to do? :");
+			
+			Console.WriteLine("		1) View Tickets");
+			Console.WriteLine("		2) View Stock");
+			Console.WriteLine("		3) Add Stock");
+			Console.WriteLine("		4) Done");
+		
+			choice = Console.ReadLine();
+
+			if (!string.IsNullOrEmpty(Convert.ToString(choice))) {
+				if (Convert.ToInt32(choice) > 4 || Convert.ToInt32(choice) < 0) {
+					Console.WriteLine("Please enter a number from 1 to 4");
+					return ViewRecords();
+				}
+			}
+		
+	
+
+		switch (choice) {
+			case("1"):
+				foreach (var ticket in _foodTruck.LoadTickets("./past/")) {
+            
+            	Console.WriteLine("-----------------------");
+            
+            	Console.Write(ticket.Get_orderid() + " : ");
+
+            	foreach (var mi in ticket.Get_menu_items()) {
+                
+            	}
+                        
+            	Console.WriteLine();
+			case("2"):
+				foreach (var item in DictionaryMenuItem) {
+					Console.WriteLine();
+				}
+            
+            return null;
+        	}
+		}
+	}
+}
+		
